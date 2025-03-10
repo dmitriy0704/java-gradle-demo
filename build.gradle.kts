11ad766b920141eb5a1e0559a7e8d74ce6593748
Dmitriy Folomkin <dmitriy.folomkin@yandex.ru>
2025-03-10 10:18:16 +0300

    ...

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     id("java")
+    base
+    `java-library`
+    application
 }
 
 group = "dev.folomkin"
@@ -10,10 +13,30 @@ repositories {
 }
 
 dependencies {
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
+    implementation("org.slf4j:slf4j-api:2.0.17")
+    runtimeOnly("org.slf4j:slf4j-simple:2.0.17")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
+application {
+    mainClass.set("dev.folomkin.Main")
+}
+
+tasks.jar {
+    manifest {
+        attributes("Main-Class" to application.mainClass.get())
+    }
+}
+
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.register("my-task") {
+    println("конфигурируем")
+    doLast {
+        println("Привет от Gradle")
+    }
 }
